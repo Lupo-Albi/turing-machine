50e7b044e0706df5f49a485fd68cd6ee6c094673
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-03-31 16:43:30 -0300

    Created function to get the index of a symbol (indiceSimbolo)

--- main.c
+++ main.c
@@ -64,3 +64,24 @@ int indiceEstado (turing_t *turing, char *estado) {
     }
     return 0;
 }
+
+/*
+ * Função: indiceSimbolo
+ * ------------------------
+ * Procura por um símbolo no conjunto de símbolos de uma máquina
+ * 
+ * *turing: a máquina de Turing a qual possui um conjunto de símbolos
+ * simbolo: símbolo que se deseja procurar no conjunto de símbolos da máquina
+ * 
+ * return: retorna o índice do símbolo. Retorna 0 caso não encontre nenhuma correspondência
+ */
+int indiceSimbolo(turing_t *turing, char simbolo) {
+    int indice;
+
+    for (indice = 0; indice < turing->simbolosComprimento; indice++) {
+        if (turing->simbolos[indice] == simbolo) {
+            return indice;
+        }
+    }
+    return 0;
+}
